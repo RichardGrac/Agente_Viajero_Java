11aa8581831511ac14232b4757d96089e7df9f5d
Ricardo Garcia <Richard_Grac@hotmail.com>
2018-06-19 19:13:54 -0500

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+/*
+* INTEGRANTES:
+*   GARCÍA NAVARRO RICARDO DANIEL
+*   GONZALEZ OLIVARES JOSUÉ
+*
+* */
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -9,15 +16,71 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Node> nodes = new ArrayList<>();
         City cities = new City();
-        for (int i = 0; i < NUM_NODES; i++){
+        for (int i = 0; i < NUM_NODES; i++) {
             nodes.add(new Node(cities));
         }
         /* Array Sorted: */
-        Collections.sort(nodes, Comparator.comparingDouble(Node::getDistance));
+//        nodes.sort(Comparator.comparingDouble(Node::getDistance));
+//        printArray("Lista 1", nodes);
 
+        /* 'nodes' best 50 results */
+//        nodes.subList(50, 100).clear();
+//        printArray("Mejores 50 [Lista 1]", nodes);
+
+        System.out.println("GENERACIÓN \t CROMOSOMA \t\t CONTENIDO \t\t RECORRIDO");
+        /* First Generation (Second List) */
+        for(int i = 0; i < 100; i++){
+            ArrayList<Node> nextGen = nextGeneration(nodes, cities);
+            nextGen.sort(Comparator.comparingDouble(Node::getDistance));
+//            printArray(("Generación #" + (i+1)), nextGen);
+
+            /* 'nextGen' best 50 results */
+            nextGen.subList(50,100).clear();
+//            printArray("Mejores 50 [Lista " + (i+1) + "]", nextGen);
+
+            /* Swap of 5° and 10° element */
+            nextGen = swap(nextGen, 4, 9);
+//            printArray("1° y 5° swapped", nextGen);
+//            System.out.println("------------------------ Elementos 1 y 5 swapped en Primer Nodo [Gen " + i + "] -------------------");
+            System.out.print("\t" + i + " \t\t " + (i+1) + "\t\t\t\t");
+            nextGen.get(0).printVector();
+            nodes = nextGen;
+        }
+
+    }
+
+
+    public static void printArray(String message, ArrayList<Node> nodes) {
+        System.out.println("------------------------------------- " + message + " ------------------------------------------");
         for (int i = 0; i < nodes.size(); i++) {
-            System.out.print("[" + (i+1) + "]: ");
+            System.out.print("[" + (i + 1) + "]: ");
             nodes.get(i).printVector();
         }
+    }
+
+    public static ArrayList<Node> nextGeneration(ArrayList<Node> nodes, City cities){
+        ArrayList<Node> nodes2 = new ArrayList<>();
+        int j = 1;
+        for (int i = 0; i < nodes.size() - 1; i = i + 2) {
+            nodes2.add(new Node(nodes.get(i), (i + 1), nodes.get(i + j), (i + j + 1), cities));
+        }
+
+        while (j < 4) {
+            j++;
+            for (int i = 0; i < 25; i++) {
+                nodes2.add(new Node(nodes.get(i), (i + 1), nodes.get(i + j), (i + j + 1), cities));
+            }
+        }
+        return nodes2;
+    }
+
+    public static ArrayList<Node> swap(ArrayList<Node> nodes, int index_a, int index_b){
+        Node firstNode = nodes.get(0);
+        int fifthlement = firstNode.getCity(index_a);
+        firstNode.setCity(index_a, firstNode.getCity(index_b));
+        firstNode.setCity(index_b, fifthlement);
+        firstNode.estimateLength();
+        nodes.set(0, firstNode);
+        return nodes;
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,7 +1,7 @@
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
-public class Node{
+public class Node {
 
     private ArrayList<Integer> vector = new ArrayList<>();
     private double distance;
@@ -9,7 +9,7 @@ public class Node{
     private ArrayList<Integer> base = new ArrayList<>();
     private City cities;
 
-    Node(City cities){
+    Node(City cities) {
         this.cities = cities;
         createBase();
         createRoute();
@@ -17,12 +17,12 @@ public class Node{
     }
 
     /*
-    *  Creamos la estructura base de elementos ordenados
-    *  i = 2 porque '1' lo agregarémos manualmente a 'vector'
-    *
-    * */
-    private void createBase(){
-        for (int i = 2; i < this.cities.getCities().size() + 1; i++){
+     *  Creamos la estructura base de elementos ordenados
+     *  i = 2 porque '1' lo agregarémos manualmente a 'vector'
+     *
+     * */
+    private void createBase() {
+        for (int i = 2; i < this.cities.getCities().size() + 1; i++) {
             base.add(i);
         }
     }
@@ -31,8 +31,8 @@ public class Node{
         // Primera ciudad siempre '1'
         vector.add(1);
         /* Creación de el recorrido de ciudades de manera aleatoria */
-        while(base.size() > 0){
-            int index = (int)(Math.random() * base.size());
+        while (base.size() > 0) {
+            int index = (int) (Math.random() * base.size());
             Integer pulled = base.get(index);
             vector.add(pulled);
             base.remove(index);
@@ -42,21 +42,22 @@ public class Node{
         vector.add(1);
     }
 
-    private void estimateLength() {
+    public void estimateLength() {
+        this.distance = 0;
         for (int i = 0; i < vector.size() - 1; i++) {
             Coordenates city1 = cities.getCity(vector.get(i) - 1);
-            Coordenates city2 = cities.getCity(vector.get(i+1) - 1);
+            Coordenates city2 = cities.getCity(vector.get(i + 1) - 1);
 
             double aux = distanceBetweenTwoCities(city1.getX(), city2.getX(), city1.getY(), city2.getY());
             this.distance += aux;
         }
     }
 
-    private double distanceBetweenTwoCities(int x1, int x2, int y1, int y2){
+    private double distanceBetweenTwoCities(int x1, int x2, int y1, int y2) {
         return Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
     }
 
-    public void printVector(){
+    public void printVector() {
         System.out.print("[ ");
         for (int i = 0; i < this.vector.size(); i++) {
             System.out.print(vector.get(i) + " ");
@@ -68,5 +69,94 @@ public class Node{
 
     public double getDistance() {
         return distance;
+    }
+
+    public int getCity(int index) {
+        return this.vector.get(index);
+    }
+
+    public void setCity (int index, int value){
+        this.vector.set(index, value);
+    }
+
+
+    /* ------- METHODS FOR SECOND PART ------------------ */
+    /*
+     * Creating a son by fathers
+     */
+    Node(Node f1, int num1, Node f2, int num2, City cities) {
+        this.cities = cities;
+//        System.out.print("\n\nPadre " + num1 + ": ");
+//        f1.printVector();
+
+//        System.out.print("Padre " + num2 + ": ");
+//        f2.printVector();
+
+        initializeVectorInZeros();
+        /* Seteamos dos primeros y dos últimos */
+        this.vector.set(0, 1);
+        this.vector.set(1, f1.getCity(1));
+        this.vector.set(24, f1.getCity(24));
+        this.vector.set(25, 1);
+
+        int lastIndex = 1;
+        int lastValue = f2.getCity(lastIndex);
+        fillWithFather1(lastIndex, lastValue, f1, f2);
+        fillWithFather2(f1, f2);
+        estimateLength();
+
+//        System.out.print("\tHijo: ");
+//        printVector();
+    }
+
+    private void initializeVectorInZeros() {
+        for (int i = 0; i < 26; i++) {
+            this.vector.add(0);
+        }
+    }
+
+    private void fillWithFather1(int lastIndex, int lastValue, Node f1, Node f2) {
+        ArrayList<Integer> existing = new ArrayList<>(); // Ciudades existentes
+        existing.add(f1.getCity(1));    // Agregamos segundo y penúltimo elemento. El '1' igual no saldrá
+        existing.add(f1.getCity(24));   // dentro de las busquedas.
+        boolean band = false;   // Condición de paro
+        int newValue;
+        do {
+            lastIndex = searchIndex(lastValue, f1);
+            newValue = f1.getCity(lastIndex);
+            if (existing.contains(newValue)) {
+                band = true;
+//                System.out.println("Arreglo de existentes: " + existing.toString());
+//                System.out.print("Pertenencientes a 1° Padre: ");
+//                printVector();
+            } else {
+                this.vector.set(lastIndex, newValue);
+                lastValue = f2.getCity(lastIndex);
+                existing.add(newValue);
+            }
+        } while (!band);
+    }
+
+    private void fillWithFather2(Node f1, Node f2) {
+        for (int i = 2; i < this.vector.size() - 2; i++) {
+            if (this.vector.get(i) == 0){
+                if ((f2.getCity(i) == f1.getCity(24)) || (f2.getCity(i) == f1.getCity(1))){
+                    this.vector.set(i, f2.getCity(24));
+                } else {
+                    this.vector.set(i, f2.getCity(i));
+                }
+            }
+        }
+    }
+
+    /* Búsqueda de indice por un valor */
+    private int searchIndex(int value, Node f1) {
+//        System.out.print("Buscando: " + value);
+        int i = 0;
+        while (f1.getCity(i) != value) {
+            i++;
+        }
+//        System.out.println(", Encontrado en i=" + i);
+        return i;
     }
 }
